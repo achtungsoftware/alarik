1ebb6c3767480148dfe460aab81bd89317faa93b
Julian Gerhards <96697201+juliangerhards@users.noreply.github.com>
2025-12-08 12:27:05 +0100

    fix: logo aria-label

--- console/app/components/logo.vue
+++ console/app/components/logo.vue
@@ -37,7 +37,7 @@ function logoWidth() {
 </script>
 
 <template>
-    <NuxtLink :prefetch="false" to="/" class="flex-shrink-0" aria-label="belegFuchs Logo">
+    <NuxtLink :prefetch="false" to="/" class="flex-shrink-0" aria-label="Alarik Logo">
         <svg :width="logoWidth()" height="100%" viewBox="0 0 4267 1067" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule: evenodd; clip-rule: evenodd; stroke-linejoin: round; stroke-miterlimit: 2">
             <g transform="matrix(0.25,0,0,0.25,585.05,533.333)">
                 <g transform="matrix(1,0,0,1,-2133.33,-2133.33)">
